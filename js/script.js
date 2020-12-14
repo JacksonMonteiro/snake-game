45a91ef621d7a18bf24a87a1f6cb2d53706badd8
jacksonmonteiro <infor.jackson324@gmail.com>
2020-12-14 13:37:43 -0300

    Snake movimentation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 let canvas = document.querySelector('#snake');
 let context = canvas.getContext('2d');
 let box = 32;
+
 let snake = [];
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+
+let direction = 'right';
 
 function createBackground() {
    context.fillStyle = 'lightgreen';
@@ -19,5 +22,26 @@ function createSnake() {
     }
 }
 
-createBackground();
-createSnake();
+function startGame() {
+    createBackground();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == 'right') snakeX += box;
+    if (direction == 'left') snakeX -= box;
+    if (direction == 'up') snakeY += box;
+    if (direction == 'down') snakeY -= box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
